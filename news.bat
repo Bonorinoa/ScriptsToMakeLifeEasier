3527afc29bdc5f1dfa76145b70a3383ad92f590a
Augusto Gonzalez Bonorino <Augusto.GonzalezBonorino1@marist.edu>
2021-06-13 17:21:30 -0400

    Update news.bat

--- news.bat
+++ news.bat
@@ -1,5 +1,8 @@
 @echo off
 
+:: This program initializes your favorite websites to save you some brain power before your morning coffee.
+:: You can specify any website you want use the following syntax as a reference to customize it:
+
 start "" https://www.google.com/
 
 start "" https://github.com/
